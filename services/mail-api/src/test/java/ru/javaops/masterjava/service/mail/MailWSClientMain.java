c929390d044e1fb14f2353161e3cb8a521807374
DariaSamoylova <daria.borodina@gmail.com>
2019-06-11 14:51:41 +0300

    Apply patch 7_9_refactoring_send_to_group.patch

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -1,14 +1,16 @@
 package ru.javaops.masterjava.service.mail;
 
-import com.google.common.collect.ImmutableList;
+        import com.google.common.collect.ImmutableList;
+        import com.google.common.collect.ImmutableSet;
 
 public class MailWSClientMain {
     public static void main(String[] args) {
-        MailWSClient.sendMail(
-
-                ImmutableList.of(new Addressee("To <fireillusion@yandex.ru>")),
-                ImmutableList.of(new Addressee("Copy <fireillusion@yandex.ru>")), "Subject", "Body");
-               // ImmutableList.of(new Addressee("To <masterjava@javaops.ru>")),
-                //ImmutableList.of(new Addressee("Copy <masterjava@javaops.ru>")), "Subject", "Body");
+         MailWSClient.sendToGroup(
+                ImmutableSet.of(new Addressee("To <masterjava@javaops.ru>")),
+                ImmutableSet.of(new Addressee("Copy <masterjava@javaops.ru>")), "Subject", "Body");
     }
 }
+      /*
+                ImmutableList.of(new Addressee("To <fireillusion@yandex.ru>")),
+                ImmutableList.of(new Addressee("Copy <fireillusion@yandex.ru>")), "Subject", "Body");
+*/
