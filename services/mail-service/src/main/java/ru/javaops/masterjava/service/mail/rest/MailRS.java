868d6fcccfdef626eae670fb8ae89ec43a09f304
DariaSamoylova <daria.borodina@gmail.com>
2019-06-28 13:31:08 +0300

    HW10 #1 attachments JAX-RS

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/rest/MailRS.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/rest/MailRS.java
@@ -1,14 +1,21 @@
 package ru.javaops.masterjava.service.mail.rest;
 
 
+import com.google.common.collect.ImmutableList;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.hibernate.validator.constraints.NotBlank;
 import ru.javaops.masterjava.service.mail.GroupResult;
 import ru.javaops.masterjava.service.mail.MailServiceExecutor;
 import ru.javaops.masterjava.service.mail.MailWSClient;
+import ru.javaops.masterjava.service.mail.util.Attachments;
 import ru.javaops.masterjava.web.WebStateException;
 
+import javax.servlet.http.Part;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 
 @Path("/")
@@ -21,12 +28,19 @@ public class MailRS {
     }
 
     @POST
-    @Path("send")
+    @Path("/send")
     @Produces(MediaType.APPLICATION_JSON)
-    public GroupResult send(@NotBlank @FormParam("users") String users,
-                            @FormParam("subject") String subject,
-                            @NotBlank @FormParam("body") String body) throws WebStateException {
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public GroupResult send(  @FormDataParam("users") String users,
+                            @FormDataParam("subject") String subject,
+                             @FormDataParam("body") String body,
+                            @FormDataParam("attach") InputStream attachStream,
+                              @FormDataParam("attach") FormDataContentDisposition fileDispos)  throws WebStateException {
 
-        return MailServiceExecutor.sendBulk(MailWSClient.split(users), subject, body, Collections.emptyList());
+
+            return MailServiceExecutor.sendBulk(MailWSClient.split(users), subject, body,
+                    attachStream == null ? null :
+                            ImmutableList.of(Attachments.getAttachment("fileTest", attachStream))) ;// Collections.emptyList());
+       
     }
 }
